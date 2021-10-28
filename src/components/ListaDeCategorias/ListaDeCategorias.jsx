4d6d32c4dd3c689642c1c011f405c9617f3b7ca0
Junior Fogassi <jrfogassi@gmail.com>
2021-10-28 20:52:43 -0300

    termino de projeto

---
+++ src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import "./estilo.css";
+class ListaDeCategorias extends Component {
+
+  constructor(){
+    super();
+    this.state={categorias:[]};
+    this.novasCategorias = this.novasCategorias.bind(this);
+  }
+  
+  componentDidMount(){
+    this.props.categorias.inscrever(this.novasCategorias);
+  }
+
+  componentWillUnmount(){
+    this.props.categorias.desinscrever(this.novasCategorias)
+  }
+
+  novasCategorias(categorias){
+    this.setState({...this.state, categorias});
+  }
+  
+  _handleEventoInput(e) {
+    if (e.key == "Enter") {
+      let valorCategoria = e.target.value;
+      this.props.adicionarCategoria(valorCategoria);
+    }
+  }
+  render() {
+    return (
+      <section className="lista-categorias">
+        <ul className="lista-categorias_lista">
+          {this.state.categorias.map((categoria, index) => {
+            return (
+              <li key={index} className="lista-categorias_item">
+                {categoria}
+              </li>
+            );
+          })}
+        </ul>
+        <input
+          type="text"
+          className="lista-categorias_input"
+          placeholder="Adicionar Categoria"
+          onKeyUp={this._handleEventoInput.bind(this)}
+        />
+      </section>
+    );
+  }
+}
+
+export default ListaDeCategorias;
